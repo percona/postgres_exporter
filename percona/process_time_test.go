ff67891a52624aa8b640661266e5b193fa4adf8c
taras.shcherban <shcherban91@gmail.com>
2022-07-27 16:30:41 +0300

    PMM-10278 Test update

--- percona/process_time_test.go
+++ percona/process_time_test.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"testing"
@@ -173,10 +174,10 @@ func checkPort(port int) bool {
 }
 
 func doTest(t *testing.T, iterations int, fileName string) (int64, error) {
-	//lines, err := os.ReadFile("test.exporter-flags.txt")
-	//if !assert.NoError(t, err, "unable to read exporter args file") {
-	//	return 0, err
-	//}
+	lines, err := os.ReadFile("test.exporter-flags.txt")
+	if !assert.NoError(t, err, "unable to read exporter args file") {
+		return 0, err
+	}
 
 	var port = -1
 	for i := portRangeStart; i < portRangeEnd; i++ {
@@ -190,18 +191,24 @@ func doTest(t *testing.T, iterations int, fileName string) (int64, error) {
 		panic(fmt.Sprintf("Failed to find free port in range [%d..%d]", portRangeStart, portRangeEnd))
 	}
 
-	//linesStr := string(lines)
-	linesStr := fmt.Sprintf("--web.listen-address=127.0.0.1:%d", port)
+	linesStr := string(lines)
+	linesStr += fmt.Sprintf("\n--web.listen-address=127.0.0.1:%d", port)
+
+	absolutePath, _ := filepath.Abs("custom-queries")
+	linesStr += fmt.Sprintf("\n--collect.custom_query.hr.directory=%s/high-resolution", absolutePath)
+	linesStr += fmt.Sprintf("\n--collect.custom_query.mr.directory=%s/medium-resolution", absolutePath)
+	linesStr += fmt.Sprintf("\n--collect.custom_query.lr.directory=%s/low-resolution", absolutePath)
+
 	linesArr := strings.Split(linesStr, "\n")
 
 	cmd := exec.Command(fileName, linesArr...)
 	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "DATA_SOURCE_NAME='postgresql://postgres:postgres@127.0.0.1:5432/postgres_exporter?sslmode=disable'")
+	cmd.Env = append(cmd.Env, "DATA_SOURCE_NAME=postgresql://postgres:postgres@127.0.0.1:5432/postgres_exporter?sslmode=disable")
 
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
-	err := cmd.Start()
+	err = cmd.Start()
 	if !assert.NoError(t, err, "Failed to start exporter. Process output:\n%q", out.String()) {
 		return 0, err
 	}
@@ -269,6 +276,56 @@ func getCPUTime(pid int) (total int64) {
 }
 
 func tryGetMetrics(port int) error {
+	uri := fmt.Sprintf("http://127.0.0.1:%d/metrics", port)
+	client := new(http.Client)
+
+	request, err := http.NewRequest("GET", uri, nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Add("Accept-Encoding", "gzip")
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		return fmt.Errorf("failed to get response from exporters web interface: %w", err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to get response from exporters web interface: %w", err)
+	}
+
+	// Check that the server actually sent compressed data
+	var reader io.ReadCloser
+	enc := response.Header.Get("Content-Encoding")
+	switch enc {
+	case "gzip":
+		reader, err = gzip.NewReader(response.Body)
+		defer reader.Close()
+	default:
+		reader = response.Body
+	}
+
+	buf := new(strings.Builder)
+	_, err = io.Copy(buf, reader)
+	if err != nil {
+		return err
+	}
+
+	rr := buf.String()
+	if rr == "" {
+		return fmt.Errorf("failed to read response")
+	}
+
+	err = response.Body.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close response: %w", err)
+	}
+
+	return nil
+}
+
+func tryGetMetrics1(port int) error {
 	resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/metrics", port))
 	if err != nil {
 		return fmt.Errorf("failed to get response from exporters web interface: %w", err)
