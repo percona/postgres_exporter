e558fa77dec9bb7ff8699cfe7c66d171cb24aa7a
taras.shcherban <shcherban91@gmail.com>
2022-10-04 16:24:04 +0200

    PMM-10512 column description

--- cmd/postgres_exporter/postgres_exporter.go
+++ cmd/postgres_exporter/postgres_exporter.go
@@ -299,8 +299,8 @@ var builtinMetricMaps = map[string]intermediateMetricMap{
 		map[string]ColumnMapping{
 			"datname":          {LABEL, "Name of this database", nil, nil},
 			"state":            {LABEL, "connection state", nil, semver.MustParseRange(">=9.2.0")},
-			"usename":          {LABEL, "connection usename", nil, nil},
-			"application_name": {LABEL, "connection application_name", nil, nil},
+			"usename":          {LABEL, "Name of the user logged into this backend", nil, nil},
+			"application_name": {LABEL, "Name of the application that is connected to this backend", nil, nil},
 			"count":            {GAUGE, "number of connections in this state", nil, nil},
 			"max_tx_duration":  {GAUGE, "max duration in seconds any active transaction has been running", nil, nil},
 		},
