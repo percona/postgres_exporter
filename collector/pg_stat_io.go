fd5646d174cd76e7841f1acc47ec6393a75f33f2
Artem Gavrilov <artem.gavrilov@percona.com>
2025-10-09 18:39:50 +0200

    Fix pg_stat_io collector

--- collector/pg_stat_io.go
+++ collector/pg_stat_io.go
@@ -39,49 +39,97 @@ func NewPGStatIOCollector(config collectorConfig) (Collector, error) {
 var (
 	statIOReads = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, statIOSubsystem, "reads_total"),
-		"Number of reads",
-		[]string{"backend_type", "io_context", "io_object"},
-		prometheus.Labels{},
-	)
-	statIOReadTime = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, statIOSubsystem, "read_time_seconds_total"),
-		"Time spent reading",
-		[]string{"backend_type", "io_context", "io_object"},
-		prometheus.Labels{},
-	)
-	statIOWrites = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, statIOSubsystem, "writes_total"),
-		"Number of writes",
-		[]string{"backend_type", "io_context", "io_object"},
-		prometheus.Labels{},
-	)
-	statIOWriteTime = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, statIOSubsystem, "write_time_seconds_total"),
-		"Time spent writing",
-		[]string{"backend_type", "io_context", "io_object"},
-		prometheus.Labels{},
-	)
-	statIOExtends = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, statIOSubsystem, "extends_total"),
-		"Number of extends",
+		"Number of read operations",
 		[]string{"backend_type", "io_context", "io_object"},
 		prometheus.Labels{},
 	)
 	statIOReadBytes = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, statIOSubsystem, "read_bytes_total"),
-		"Number of bytes read (PostgreSQL 18+)",
+		"The total size of read operations, in bytes",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOReadTime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "read_time_total"),
+		"Time spent waiting for read operations, in milliseconds",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOWrites = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "writes_total"),
+		"Number of write operations",
 		[]string{"backend_type", "io_context", "io_object"},
 		prometheus.Labels{},
 	)
 	statIOWriteBytes = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, statIOSubsystem, "write_bytes_total"),
-		"Number of bytes written (PostgreSQL 18+)",
+		"The total size of write operations, in bytes",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOWriteTime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "write_time_total"),
+		"Time spent waiting for write operations, in milliseconds",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOWritebacks = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "writebacks_total"),
+		"Number of units of size BLCKSZ (typically 8kB) which the process requested the kernel write out to permanent storage",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOWritebackTime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "writeback_time_total"),
+		"Time spent waiting for writeback operations, in milliseconds",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOExtends = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "extends_total"),
+		"Number of extend operations",
 		[]string{"backend_type", "io_context", "io_object"},
 		prometheus.Labels{},
 	)
 	statIOExtendBytes = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, statIOSubsystem, "extend_bytes_total"),
-		"Number of bytes extended (PostgreSQL 18+)",
+		"The total size of extend operations, in bytes",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOExtendTime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "extend_time_total"),
+		"Time spent waiting for extend operations, in milliseconds",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOHits = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "hits_total"),
+		"The number of times a desired block was found in a shared buffer",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOEvictions = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "evictions_total"),
+		"Number of times a block has been written out from a shared or local buffer in order to make it available for another use",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOReueses = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "reueses_total"),
+		"The number of times an existing buffer in a size-limited ring buffer outside of shared buffers was reused as part of an I/O operation in the bulkread, bulkwrite, or vacuum contexts",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOFsyncs = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "fsyncs_total"),
+		"Number of fsync calls",
+		[]string{"backend_type", "io_context", "io_object"},
+		prometheus.Labels{},
+	)
+	statIOFsyncTime = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, statIOSubsystem, "fsync_time_total"),
+		"Time spent waiting for fsync operations, in milliseconds",
 		[]string{"backend_type", "io_context", "io_object"},
 		prometheus.Labels{},
 	)
@@ -90,16 +138,24 @@ var (
 	StatIOQuery18Plus = `
 		SELECT
 			backend_type,
-			io_context,
 			io_object,
+			io_context,
 			reads,
+			read_bytes,
 			read_time,
 			writes,
-			write_time,
-			extends,
-			read_bytes,
 			write_bytes,
-			extend_bytes
+			write_time,
+			writebacks,
+			writeback_time,
+			extends,
+			extend_bytes,
+			extend_time,
+			hits,
+			evictions,
+			reueses,
+			fsyncs
+			fsync_time,
 		FROM pg_stat_io
 	`
 
@@ -107,16 +163,24 @@ var (
 	StatIOQueryPre18 = `
 		SELECT
 			backend_type,
-			io_context,
 			io_object,
+			io_context,
 			reads,
+			NULL::bigint as read_bytes,
 			read_time,
 			writes,
-			write_time,
-			extends,
-			NULL::bigint as read_bytes,
 			NULL::bigint as write_bytes,
-			NULL::bigint as extend_bytes
+			write_time,
+			writebacks,
+			writeback_time,
+			extends,
+			NULL::numeric as extend_bytes,
+			extend_time,
+			hits,
+			NULL::bigint as evictions,
+			reueses,
+			fsyncs
+			fsync_time
 		FROM pg_stat_io
 	`
 )
@@ -129,9 +193,10 @@ func (c *PGStatIOCollector) Update(ctx context.Context, instance *instance, ch c
 
 	db := instance.getDB()
 
+	after18 := instance.version.GTE(semver.Version{Major: 18})
 	// Use version-specific query for PostgreSQL 18+
 	query := StatIOQueryPre18
-	if instance.version.GTE(semver.Version{Major: 18}) {
+	if after18 {
 		query = StatIOQuery18Plus
 	}
 
@@ -143,21 +208,29 @@ func (c *PGStatIOCollector) Update(ctx context.Context, instance *instance, ch c
 
 	for rows.Next() {
 		var backendType, ioContext, ioObject sql.NullString
-		var reads, writes, extends, readBytes, writeBytes, extendBytes sql.NullFloat64
-		var readTime, writeTime sql.NullFloat64
+		var reads, writes, writebacks, extends, hits, evictions, reueses, fsyncs sql.NullInt64
+		var readBytes, writeBytes, extendBytes, readTime, writeTime, extendTime, writebackTime, fsyncTime sql.NullFloat64
 
 		err := rows.Scan(
 			&backendType,
-			&ioContext,
 			&ioObject,
+			&ioContext,
 			&reads,
+			&readBytes,
 			&readTime,
 			&writes,
-			&writeTime,
-			&extends,
-			&readBytes,
 			&writeBytes,
+			&writeTime,
+			&writebacks,
+			&writebackTime,
+			&extends,
 			&extendBytes,
+			&extendTime,
+			&hits,
+			&evictions,
+			&reueses,
+			&fsyncs,
+			&fsyncTime,
 		)
 		if err != nil {
 			return err
@@ -167,88 +240,188 @@ func (c *PGStatIOCollector) Update(ctx context.Context, instance *instance, ch c
 		if backendType.Valid {
 			backendTypeLabel = backendType.String
 		}
-		ioContextLabel := "unknown"
-		if ioContext.Valid {
-			ioContextLabel = ioContext.String
-		}
 		ioObjectLabel := "unknown"
 		if ioObject.Valid {
 			ioObjectLabel = ioObject.String
 		}
+		ioContextLabel := "unknown"
+		if ioContext.Valid {
+			ioContextLabel = ioContext.String
+		}
 
 		labels := []string{backendTypeLabel, ioContextLabel, ioObjectLabel}
 
+		readsMetric := 0.0
 		if reads.Valid {
-			ch <- prometheus.MustNewConstMetric(
-				statIOReads,
-				prometheus.CounterValue,
-				reads.Float64,
-				labels...,
-			)
+			readsMetric = float64(reads.Int64)
 		}
+		ch <- prometheus.MustNewConstMetric(
+			statIOReads,
+			prometheus.CounterValue,
+			readsMetric,
+			labels...,
+		)
 
 		if readTime.Valid {
 			ch <- prometheus.MustNewConstMetric(
 				statIOReadTime,
 				prometheus.CounterValue,
-				readTime.Float64/1000.0, // Convert milliseconds to seconds
+				readTime.Float64,
 				labels...,
 			)
 		}
 
+		writesMetric := 0.0
 		if writes.Valid {
-			ch <- prometheus.MustNewConstMetric(
-				statIOWrites,
-				prometheus.CounterValue,
-				writes.Float64,
-				labels...,
-			)
+			writesMetric = float64(writes.Int64)
 		}
+		ch <- prometheus.MustNewConstMetric(
+			statIOWrites,
+			prometheus.CounterValue,
+			writesMetric,
+			labels...,
+		)
 
 		if writeTime.Valid {
 			ch <- prometheus.MustNewConstMetric(
 				statIOWriteTime,
 				prometheus.CounterValue,
-				writeTime.Float64/1000.0, // Convert milliseconds to seconds
+				writeTime.Float64,
 				labels...,
 			)
 		}
 
+		writebacksMetric := 0.0
+		if writebacks.Valid {
+			writebacksMetric = float64(writebacks.Int64)
+		}
+		if writebacks.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOWritebacks,
+				prometheus.CounterValue,
+				writebacksMetric,
+				labels...,
+			)
+		}
+
+		if writebackTime.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOWritebackTime,
+				prometheus.CounterValue,
+				writebackTime.Float64,
+				labels...,
+			)
+		}
+
+		extendsMetric := 0.0
+		if extends.Valid {
+			extendsMetric = float64(extends.Int64)
+		}
 		if extends.Valid {
 			ch <- prometheus.MustNewConstMetric(
 				statIOExtends,
 				prometheus.CounterValue,
-				extends.Float64,
+				extendsMetric,
+				labels...,
+			)
+		}
+
+		if extendTime.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOExtendTime,
+				prometheus.CounterValue,
+				extendTime.Float64,
+				labels...,
+			)
+		}
+
+		hitsMetric := 0.0
+		if hits.Valid {
+			hitsMetric = float64(hits.Int64)
+		}
+		if hits.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOHits,
+				prometheus.CounterValue,
+				hitsMetric,
+				labels...,
+			)
+		}
+		evictionsMetric := 0.0
+		if evictions.Valid {
+			evictionsMetric = float64(evictions.Int64)
+		}
+		if evictions.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOEvictions,
+				prometheus.CounterValue,
+				evictionsMetric,
+				labels...,
+			)
+		}
+		reuesesMetric := 0.0
+		if reueses.Valid {
+			reuesesMetric = float64(reueses.Int64)
+		}
+		if reueses.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOReueses,
+				prometheus.CounterValue,
+				reuesesMetric,
+				labels...,
+			)
+		}
+
+		fsyncsMetric := 0.0
+		if fsyncs.Valid {
+			fsyncsMetric = float64(fsyncs.Int64)
+		}
+		if fsyncs.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOFsyncs,
+				prometheus.CounterValue,
+				fsyncsMetric,
+				labels...,
+			)
+		}
+
+		if fsyncTime.Valid {
+			ch <- prometheus.MustNewConstMetric(
+				statIOFsyncTime,
+				prometheus.CounterValue,
+				fsyncTime.Float64,
 				labels...,
 			)
 		}
 
 		// PostgreSQL 18+ byte statistics
-		if readBytes.Valid {
-			ch <- prometheus.MustNewConstMetric(
-				statIOReadBytes,
-				prometheus.CounterValue,
-				readBytes.Float64,
-				labels...,
-			)
-		}
+		if after18 {
+			if readBytes.Valid {
+				ch <- prometheus.MustNewConstMetric(
+					statIOReadBytes,
+					prometheus.CounterValue,
+					readBytes.Float64,
+					labels...,
+				)
+			}
 
-		if writeBytes.Valid {
-			ch <- prometheus.MustNewConstMetric(
-				statIOWriteBytes,
-				prometheus.CounterValue,
-				writeBytes.Float64,
-				labels...,
-			)
-		}
+			if writeBytes.Valid {
+				ch <- prometheus.MustNewConstMetric(
+					statIOWriteBytes,
+					prometheus.CounterValue,
+					writeBytes.Float64,
+					labels...,
+				)
+			}
 
-		if extendBytes.Valid {
-			ch <- prometheus.MustNewConstMetric(
-				statIOExtendBytes,
-				prometheus.CounterValue,
-				extendBytes.Float64,
-				labels...,
-			)
+			if extendBytes.Valid {
+				ch <- prometheus.MustNewConstMetric(
+					statIOExtendBytes,
+					prometheus.CounterValue,
+					extendBytes.Float64,
+					labels...,
+				)
+			}
 		}
 	}
 
--- collector/pg_stat_io_test.go
+++ collector/pg_stat_io_test.go
@@ -20,6 +20,8 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/blang/semver/v4"
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+	"github.com/smartystreets/goconvey/convey"
 )
 
 func TestPGStatIOCollector(t *testing.T) {
@@ -31,9 +33,9 @@ func TestPGStatIOCollector(t *testing.T) {
 
 	inst := &instance{db: db, version: semver.MustParse("16.0.0")}
 
-	columns := []string{"backend_type", "io_context", "io_object", "reads", "read_time", "writes", "write_time", "extends", "read_bytes", "write_bytes", "extend_bytes"}
+	columns := []string{"backend_type", "io_object", "io_context",  "reads", "read_bytes", "read_time", "writes", "write_bytes", "write_time", "writebacks", "writeback_time", "extends", "extend_bytes", "extend_time", "hits", "evictions", "reueses", "fsyncs", "fsync_time"}
 	rows := sqlmock.NewRows(columns).
-		AddRow("client backend", "normal", "relation", 100, 50.5, 75, 25.2, 10, nil, nil, nil)
+		AddRow("client backend", "relation", "normal", 100, nil, 50.5, 75 ,nil, 25.2, 10, 12.0, 7, nil, 11.0, 1 ,2 ,3, 4, 8.0 )
 	mock.ExpectQuery("SELECT.*backend_type.*FROM pg_stat_io").WillReturnRows(rows)
 
 	ch := make(chan prometheus.Metric)
@@ -46,20 +48,31 @@ func TestPGStatIOCollector(t *testing.T) {
 		}
 	}()
 
-	expected := 5 // reads, read_time, writes, write_time, extends (no byte metrics for v16)
-
-	metricCount := 0
-	for m := range ch {
-		metricCount++
-		_ = m
+	labels := labelMap{"backend_type": "client backend", "io_object": "relation", "io_context": "normal",}
+	expected := []MetricResult{
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 100},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 50.5},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 75},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 25.2},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 10},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 12.0},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 7},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 11.0},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 1},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 2},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 3},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 4},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 8.0},
 	}
 
-	if metricCount != expected {
-		t.Errorf("Expected %d metrics, got %d", expected, metricCount)
-	}
-
+	convey.Convey("Metrics comparison", t, func() {
+		for _, expect := range expected {
+			m := readMetric(<-ch)
+			convey.So(expect, convey.ShouldResemble, m)
+		}
+	})
 	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
+		t.Errorf("there were unfulfilled exceptions: %s", err)
 	}
 }
 
@@ -70,11 +83,11 @@ func TestPGStatIOCollectorPostgreSQL18(t *testing.T) {
 	}
 	defer db.Close()
 
-	inst := &instance{db: db, version: semver.MustParse("18.0.0")}
+	inst := &instance{db: db, version: pg18}
 
-	columns := []string{"backend_type", "io_context", "io_object", "reads", "read_time", "writes", "write_time", "extends", "read_bytes", "write_bytes", "extend_bytes"}
+	columns := []string{"backend_type", "io_context", "io_object", "reads", "read_bytes", "read_time", "writes", "write_bytes", "write_time", "writebacks", "writeback_time", "extends", "extend_bytes", "extend_time", "hits", "evictions", "reueses", "fsyncs", "fsync_time"}
 	rows := sqlmock.NewRows(columns).
-		AddRow("client backend", "normal", "relation", 100, 50.5, 75, 25.2, 10, 1024, 2048, 512)
+		AddRow("client backend", "relation", "normal", 100, 90, 50.5, 75, 80, 25.2, 10, 12.0, 7, 30, 11.0, 1, 2, 3, 4, 8.0)
 	mock.ExpectQuery("SELECT.*backend_type.*FROM pg_stat_io").WillReturnRows(rows)
 
 	ch := make(chan prometheus.Metric)
@@ -87,20 +100,34 @@ func TestPGStatIOCollectorPostgreSQL18(t *testing.T) {
 		}
 	}()
 
-	expected := 8 // reads, read_time, writes, write_time, extends, read_bytes, write_bytes, extend_bytes
-
-	metricCount := 0
-	for m := range ch {
-		metricCount++
-		_ = m
+	labels := labelMap{"backend_type": "client backend", "io_object": "relation", "io_context": "normal",}
+	expected := []MetricResult{
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 100},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 50.5},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 75},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 25.2},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 10},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 12.0},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 7},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 11.0},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 1},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 2},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 3},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 4},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 8.0},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 90},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 80},
+		{labels: labels, metricType: dto.MetricType_COUNTER, value: 30},
 	}
 
-	if metricCount != expected {
-		t.Errorf("Expected %d metrics, got %d", expected, metricCount)
-	}
-
+	convey.Convey("Metrics comparison", t, func() {
+		for _, expect := range expected {
+			m := readMetric(<-ch)
+			convey.So(expect, convey.ShouldResemble, m)
+		}
+	})
 	if err := mock.ExpectationsWereMet(); err != nil {
-		t.Errorf("There were unfulfilled expectations: %s", err)
+		t.Errorf("there were unfulfilled exceptions: %s", err)
 	}
 }
 
@@ -128,13 +155,7 @@ func TestPGStatIOCollectorPrePostgreSQL16(t *testing.T) {
 		t.Errorf("There were unfulfilled expectations: %s", err)
 	}
 
-	metricCount := 0
-	for m := range ch {
-		metricCount++
-		_ = m
-	}
-
-	if metricCount != 0 {
-		t.Errorf("Expected 0 metrics for PostgreSQL < 16, got %d", metricCount)
+	for _ = range ch {
+		t.Error("Don't expect any metrics for PostgreSQL < 16")
 	}
 }
